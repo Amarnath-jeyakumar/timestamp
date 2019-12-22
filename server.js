8567def13be0156876c82a2113b2e79b78ead386
Amarnath-jeyakumar <amarnath.jeyakumar@gmail.com>
2019-12-22 22:08:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/api/timestamp/:date_string?', (req,res)=>{
   if(date){
     res.json({unix: date.getTime(), utc: date.toUTCString()});
   } else {
-    res.json({unix: null, utc: 'Invalid Date'})
+    res.json({error: 'Invalid Date'})
   }
 });
 
